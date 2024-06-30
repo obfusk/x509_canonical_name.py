eae0eef21ff6a7a674d2114d8996e5357e88a07d
FC (Fay) Stegerman <flx@obfusk.net>
2024-06-30 19:51:40 +0200

    ...

--- canonical_name.py
+++ canonical_name.py
@@ -4,36 +4,30 @@ import unicodedata
 
 from typing import Any
 
-from asn1crypto import x509                 # type: ignore[import-untyped]
+from asn1crypto import x509                     # type: ignore[import-untyped]
 
 
 def canonical_name(name: Any) -> str:
-    abbr = dict(                            # FIXME: incomplete
+    abbr = dict(                                # FIXME: incomplete
         common_name="cn",
         country_name="c",
         organizational_unit_name="ou",
     )
-    esc = {ord(c): f"\\{c}" for c in ",;"}  # FIXME: incomplete
+    esc = {ord(c): f"\\{c}" for c in ",;+"}     # FIXME: incomplete
     data = []
     for rdn in reversed(name.chosen):
-        x = []
+        pairs = []
         for type_val in rdn:
-            t = abbr.get(type_val['type'].native, type_val['type'].human_friendly.lower())
-            v = type_val['value'].native
-            if not v.isspace():
-                v = re.sub(r"\s+", " ", v).strip()
+            t = abbr.get(type_val['type'].native, type_val['type'].native)
+            v = re.sub(r"\s+", " ", type_val['value'].native).strip()
             v = unicodedata.normalize("NFKD", v.translate(esc).upper().lower())
-            x.append(f"{t}={v}")
-        data.append(sorted(x))
-    return ",".join("+".join(x) for x in data)
+            pairs.append(f"{t}={v}")
+        data.append(sorted(pairs))
+    return ",".join("+".join(pairs) for pairs in data)
 
 
 d = open(sys.argv[1], "rb").read()
 c = x509.Certificate.load(d)
 n = c["tbs_certificate"]["issuer"]
-
-# print(n.native)
-# print(n.human_friendly)
-# print(unicodedata.normalize("NFKD", n.human_friendly.upper().lower()))
 
 print(canonical_name(n))
