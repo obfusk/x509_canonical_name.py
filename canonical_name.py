b062d6623e237205964c5251656216d085dad1bb
FC (Fay) Stegerman <flx@obfusk.net>
2024-10-28 18:01:19 +0100

    ...

--- canonical_name.py
+++ canonical_name.py
@@ -8,25 +8,65 @@ import re
 import sys
 import unicodedata
 
-from typing import Any, List, Tuple, Union
+from typing import List, Tuple, Union
 
-from asn1crypto import x509                     # type: ignore[import-untyped]
-
-
-def canonical_name(name: Any) -> str:
-    """Canonical representation of x509.Name as str."""
-    return ",".join("+".join(f"{t}={v}" for _, t, v in avas) for avas in comparison_name(name))
+from asn1crypto import x509                         # type: ignore[import-untyped]
 
 
-# FIXME: leading zeroes? anything missing?
-def comparison_name(name: Any, android: bool = False) -> List[List[Tuple[int, str, str]]]:
+def x509_canonical_name(name: x509.Name, *,         # type: ignore[no-any-unimported]
+                        android: bool = False) -> str:
+    r"""
+    Canonical representation of x509.Name as str (with raw control characters
+    in places those are not stripped by normalisation).
+
+    >>> name = {"common_name": " Foo  Bar", "organization_name": "My\x00Org\b"}
+    >>> x509_canonical_name(x509.Name.build(name))
+    'cn=foo bar,o=my\x00org'
+
+    """
+    return ",".join("+".join(f"{t}={v}" for t, v in avas)
+                    for avas in x509_comparison_name(name, android=android))
+
+
+def x509_friendly_name(name: x509.Name, *,          # type: ignore[no-any-unimported]
+                       android: bool = False) -> str:
+    r"""
+    Friendly representation of x509.Name as str (with backslash escapes, no
+    normalisation, canonical ordering but not canonical representation).
+
+    >>> name = {"common_name": " Foo  Bar", "organization_name": "My\x00Org\b"}
+    >>> x509_friendly_name(x509.Name.build(name))
+    'CN= Foo  Bar, O=My\\x00Org\\x08'
+
+    """
+    return ", ".join("+".join(f"{t.upper()}={repr(rv)[1:-1]}" for _, t, _, rv in avas)
+                     for avas in x509_ordered_name(name, android=android))
+
+
+def x509_comparison_name(name: x509.Name, *,        # type: ignore[no-any-unimported]
+                         android: bool = False) -> List[List[Tuple[str, str]]]:
     r"""
     Canonical representation of x509.Name as nested list.
 
+    Returns a list of RDNs which are a list of AVAs which are a (type, value)
+    tuple, where type is the standard name or dotted OID, and value is the
+    normalised string representation of the value.
+    """
+    return [[(t, nv) for _, t, nv, _ in avas] for avas in x509_ordered_name(name, android=android)]
+
+
+# FIXME: leading zeroes? anything missing?
+def x509_ordered_name(name: x509.Name, *,           # type: ignore[no-any-unimported]
+                      android: bool = False) -> List[List[Tuple[int, str, str, str]]]:
+    r"""
+    Representation of x509.Name as nested list, in canonical ordering (but also
+    including non-canonical pre-normalised string values).
+
     Returns a list of RDNs which are a list of AVAs which are a (oid, type,
-    value) tuple, where oid is 0 for standard names and 1 for dotted OIDs, type
-    is the standard name or dotted OID, and value is the string representation
-    of the value.
+    normalised_value, raw_value) tuple, where oid is 0 for standard names and 1
+    for dotted OIDs, type is the standard name or dotted OID, normalised_value
+    is the normalised string representation of the value, and raw_value is the
+    string value pre-normalisation.
 
     https://docs.oracle.com/en/java/javase/11/docs/api/java.base/javax/security/auth/x500/X500Principal.html#getName(java.lang.String)
     https://android.googlesource.com/platform/libcore/+/refs/heads/android14-release/ojluni/src/main/java/sun/security/x509/RDN.java#481
@@ -35,23 +75,27 @@ def comparison_name(name: Any, android: bool = False) -> List[List[Tuple[int, st
     '/OU=a/CN= Foo+CN=bar /OU=b/C=xx'
     >>> data = b'0A1\n0\x08\x06\x03U\x04\x0b\x0c\x01a1\x1a0\x0b\x06\x03U\x04\x03\x0c\x04 Foo0\x0b\x06\x03U\x04\x03\x0c\x04bar 1\n0\x08\x06\x03U\x04\x0b\x0c\x01b1\x0b0\t\x06\x03U\x04\x06\x13\x02xx'
     >>> name = x509.Name.load(data)
-    >>> canonical_name(name)
+    >>> x509_canonical_name(name)
     'c=xx,ou=b,cn=bar+cn=foo,ou=a'
-    >>> comparison_name(name)
-    [[(0, 'c', 'xx')], [(0, 'ou', 'b')], [(0, 'cn', 'bar'), (0, 'cn', 'foo')], [(0, 'ou', 'a')]]
+    >>> x509_friendly_name(name)
+    'C=xx, OU=b, CN=bar +CN= Foo, OU=a'
+    >>> x509_ordered_name(name)
+    [[(0, 'c', 'xx', 'xx')], [(0, 'ou', 'b', 'b')], [(0, 'cn', 'bar', 'bar '), (0, 'cn', 'foo', ' Foo')], [(0, 'ou', 'a', 'a')]]
 
-    '/custom2=42+custom1=37+O=org/emailAddress=x@y/CN= x \t\t 猫x/CN=foo  +CN=Ii   +CN=İı +CN=ẞß+CN=bar  +CN=zz+CN= #,;\+\\/CN=#y'
+    '/custom2=42+custom1=37+O=org/emailAddress=x@y/CN= x \t\t 猫x/CN=foo  +CN=Ii   +CN=İı +CN=\u1e9eß+CN=bar  +CN=zz+CN= #,;\+\\/CN=#y'
     >>> data = b'0\x81\xbc1"0\t\x06\x03\x01\x02\x03\x0c\x02370\t\x06\x03\x01\x0b\x03\x0c\x02420\n\x06\x03U\x04\n\x0c\x03org1\x120\x10\x06\t*\x86H\x86\xf7\r\x01\t\x01\x16\x03x@y1\x130\x11\x06\x03U\x04\x03\x0c\n x \t\t \xe7\x8c\xabx1`0\t\x06\x03U\x04\x03\x0c\x02zz0\x0c\x06\x03U\x04\x03\x0c\x05Ii   0\x0c\x06\x03U\x04\x03\x0c\x05bar  0\x0c\x06\x03U\x04\x03\x0c\x05foo  0\x0c\x06\x03U\x04\x03\x0c\x05\xc4\xb0\xc4\xb1 0\x0c\x06\x03U\x04\x03\x0c\x05\xe1\xba\x9e\xc3\x9f0\r\x06\x03U\x04\x03\x0c\x06 #,;+\\1\x0b0\t\x06\x03U\x04\x03\x0c\x02#y'
     >>> name = x509.Name.load(data)
-    >>> canonical_name(name)
+    >>> x509_canonical_name(name)
     'cn=\\#y,cn=#\\,\\;\\+\\\\+cn=bar+cn=foo+cn=ii+cn=i\u0307i+cn=zz+cn=ßss,cn=x \t\t 猫x,1.2.840.113549.1.9.1=#1603784079,o=org+0.1.11.3=#0c023432+0.1.2.3=#0c023337'
-    >>> comparison_name(name)
-    [[(0, 'cn', '\\#y')], [(0, 'cn', '#\\,\\;\\+\\\\'), (0, 'cn', 'bar'), (0, 'cn', 'foo'), (0, 'cn', 'ii'), (0, 'cn', 'i\u0307i'), (0, 'cn', 'zz'), (0, 'cn', 'ßss')], [(0, 'cn', 'x \t\t 猫x')], [(1, '1.2.840.113549.1.9.1', '#1603784079')], [(0, 'o', 'org'), (1, '0.1.11.3', '#0c023432'), (1, '0.1.2.3', '#0c023337')]]
-    >>> comparison_name(name, android=True)
-    [[(0, 'cn', '\\#y')], [(0, 'cn', '#\\,\\;\\+\\\\'), (0, 'cn', 'bar'), (0, 'cn', 'foo'), (0, 'cn', 'ii'), (0, 'cn', 'i\u0307i'), (0, 'cn', 'zz'), (0, 'cn', 'ßss')], [(0, 'cn', 'x \t\t 猫x')], [(1, '1.2.840.113549.1.9.1', '#1603784079')], [(0, 'o', 'org'), (1, '0.1.2.3', '#0c023337'), (1, '0.1.11.3', '#0c023432')]]
+    >>> x509_friendly_name(name)
+    'CN=\\\\#y, CN= #\\\\,\\\\;\\\\+\\\\\\\\+CN=bar  +CN=foo  +CN=Ii   +CN=İı +CN=zz+CN=\u1e9eß, CN= x \\t\\t 猫x, 1.2.840.113549.1.9.1=#1603784079, O=org+0.1.11.3=#0c023432+0.1.2.3=#0c023337'
+    >>> x509_ordered_name(name)
+    [[(0, 'cn', '\\#y', '\\#y')], [(0, 'cn', '#\\,\\;\\+\\\\', ' #\\,\\;\\+\\\\'), (0, 'cn', 'bar', 'bar  '), (0, 'cn', 'foo', 'foo  '), (0, 'cn', 'ii', 'Ii   '), (0, 'cn', 'i\u0307i', 'İı '), (0, 'cn', 'zz', 'zz'), (0, 'cn', 'ßss', '\u1e9eß')], [(0, 'cn', 'x \t\t 猫x', ' x \t\t 猫x')], [(1, '1.2.840.113549.1.9.1', '#1603784079', '#1603784079')], [(0, 'o', 'org', 'org'), (1, '0.1.11.3', '#0c023432', '#0c023432'), (1, '0.1.2.3', '#0c023337', '#0c023337')]]
+    >>> x509_ordered_name(name, android=True)
+    [[(0, 'cn', '\\#y', '\\#y')], [(0, 'cn', '#\\,\\;\\+\\\\', ' #\\,\\;\\+\\\\'), (0, 'cn', 'bar', 'bar  '), (0, 'cn', 'foo', 'foo  '), (0, 'cn', 'ii', 'Ii   '), (0, 'cn', 'i\u0307i', 'İı '), (0, 'cn', 'zz', 'zz'), (0, 'cn', 'ßss', '\u1e9eß')], [(0, 'cn', 'x \t\t 猫x', ' x \t\t 猫x')], [(1, '1.2.840.113549.1.9.1', '#1603784079', '#1603784079')], [(0, 'o', 'org', 'org'), (1, '0.1.2.3', '#0c023337', '#0c023337'), (1, '0.1.11.3', '#0c023432', '#0c023432')]]
 
     >>> control = "".join(chr(c) for c in list(range(0x00, 0x1F + 1)))
-    >>> f = lambda cn: canonical_name(x509.Name.build({"common_name": cn}))
+    >>> f = lambda cn: x509_canonical_name(x509.Name.build({"common_name": cn}))
     >>> f(f"{control}foo") == "cn=foo" == f(f"foo{control}")
     True
     >>> f(f"\tfoo{control}bar  ")
@@ -60,11 +104,11 @@ def comparison_name(name: Any, android: bool = False) -> List[List[Tuple[int, st
     'cn=\x7f\x00foo \x00bar   '
 
     """
-    def key(pair: Tuple[int, str, str]) -> Tuple[int, Union[str, List[int]], str]:
-        o, t, v = pair
+    def key(ava: Tuple[int, str, str, str]) -> Tuple[int, Union[str, List[int]], str]:
+        o, t, nv, _ = ava
         if android and o:
-            return o, [int(x) for x in t.split(".")], v
-        return pair
+            return o, [int(x) for x in t.split(".")], nv
+        return o, t, nv
     DS, U8, PS = x509.DirectoryString, x509.UTF8String, x509.PrintableString
     oids = {
         "2.5.4.3": ("common_name", "cn"),
@@ -89,13 +133,13 @@ def comparison_name(name: Any, android: bool = False) -> List[List[Tuple[int, st
             else:
                 o, t = 1, at.dotted
             if not (isinstance(av, DS) and isinstance(av.chosen, (U8, PS))):
-                v = "#" + binascii.hexlify(av.dump()).decode()
+                rv = nv = "#" + binascii.hexlify(av.dump()).decode()
             else:
-                v = (av.native or "").translate(esc)
-                if v.startswith("#"):
-                    v = "\\" + v
-                v = unicodedata.normalize("NFKD", re.sub(r" +", " ", v).strip(cws).upper().lower())
-            avas.append((o, t, v))
+                rv = (av.native or "").translate(esc)
+                if rv.startswith("#"):
+                    rv = "\\" + rv
+                nv = unicodedata.normalize("NFKD", re.sub(r" +", " ", rv).strip(cws).upper().lower())
+            avas.append((o, t, nv, rv))
         data.append(sorted(avas, key=key))
     return data
 
@@ -103,4 +147,4 @@ def comparison_name(name: Any, android: bool = False) -> List[List[Tuple[int, st
 if __name__ == "__main__":
     with open(sys.argv[1], "rb") as fh:
         cert = x509.Certificate.load(fh.read())
-    print(canonical_name(cert["tbs_certificate"]["issuer"]))
+    print(x509_canonical_name(cert["tbs_certificate"]["issuer"]))
