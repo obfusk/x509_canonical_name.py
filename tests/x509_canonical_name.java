52c3ca083dc892538077bf337ce4a015e09c26d4
FC (Fay) Stegerman <flx@obfusk.net>
2024-10-30 00:40:43 +0100

    x509_canonical_name.java: cleaner indent

--- tests/x509_canonical_name.java
+++ tests/x509_canonical_name.java
@@ -4,17 +4,17 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
-public class JavaCert {
-  public static void main(String[] args) {
-    try {
-      FileInputStream i = new FileInputStream(args[0]);
-      X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509")
-        .generateCertificate(i);
-      String name = cert.getIssuerX500Principal().getName(
-        javax.security.auth.x500.X500Principal.CANONICAL);
-      System.out.println(name);
-    } catch (ArrayIndexOutOfBoundsException | FileNotFoundException | CertificateException e) {
-      System.err.println(e);
+public class X509CanonicalName {
+    public static void main(String[] args) {
+        try {
+            FileInputStream i = new FileInputStream(args[0]);
+            X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509")
+                .generateCertificate(i);
+            String name = cert.getIssuerX500Principal()
+                .getName(javax.security.auth.x500.X500Principal.CANONICAL);
+            System.out.println(name);
+        } catch (ArrayIndexOutOfBoundsException | FileNotFoundException | CertificateException e) {
+            System.err.println(e);
+        }
     }
-  }
 }
